bb28a9486f44c5b1f1d0e253cd19aac8d905c4ce
Taras-Melnychenko <tarasmelnychenko@gmail.com>
2016-08-19 16:17:09 +0300

    first project

---
+++ src/TestName.java
@@ -0,0 +1,15 @@
+
+
+public class TestName {
+
+
+    public static void main(String[] args) { //psvm
+
+        System.out.print("Test ...1");
+
+        System.out.println(" Test ...2"); //sout
+
+    }
+
+
+}
